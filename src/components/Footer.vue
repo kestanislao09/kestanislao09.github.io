e1d2fe1198fa0222f2c6dd3aa7de0cee5030b466
Keith Estanislao <kestanislao09@outlook.com>
2022-11-27 01:14:17 -0800

    Template skeleton

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer id="footer">
+    <div class="inner">
+      <ul class="icons">
+        <li><a href="#"><span>Twitter</span></a></li>
+        <li><a href="#"><span>Github</span></a></li>
+        <li><a href="#"><span>Email</span></a></li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
