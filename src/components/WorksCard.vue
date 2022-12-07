f822d22d84daae807f83adb181c7590ee70952d5
Keith Estanislao <kestanislao09@outlook.com>
2022-12-07 03:21:32 -0800

    git fixes

---
+++ src/components/WorksCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <a :href="item.href"><img :src="item.image" alt="" /></a>
+    <h3>{{ item.title }}</h3>
+    <p>{{ item.description }}</p>
+</template>
+
+<script setup>
+const props = defineProps(['item'])
+</script>
+
+
