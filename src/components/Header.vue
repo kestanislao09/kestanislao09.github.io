dd452dfad03d41a89bd091993f718e09fa282f18
Keith Estanislao <kestanislao09@outlook.com>
2022-12-10 04:21:24 -0800

    more styling fixes...

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col items-center lg:items-end lg:mb-auto max-w-sm mx-auto">
+  <div class="flex flex-col items-center lg:items-end lg:mb-auto max-w-sm mx-auto lg:m-0">
     <div class="bg-slate-600 h-20 w-20 mb-6 rounded-full">
       <img class="h-full w-full rounded-full" href="#"/>
     </div>
-    <h1 class="bg-black/60 lg:bg-black/0 rounded px-8 lg:px-0 text-center lg:text-right text-2xl lg:text-xl text-gray-200"><strong>I am Keith Estanislao.</strong> Dev, gamer, and father.</h1>
+    <h1 class="bg-black/60 lg:bg-black/0 rounded lg:px-0 text-center lg:text-right text-2xl lg:text-xl text-gray-200"><strong>I am Keith Estanislao.</strong><br> Dev, gamer, and father.</h1>
   </div>
 </template>
 
