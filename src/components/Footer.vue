35969fd05eff3f8fbe243fbc69d6198d37afce96
Keith Estanislao <kestanislao09@outlook.com>
2022-12-10 03:33:50 -0800

    lg breakpoint styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex lg:flex-col justify-center items-center lg:items-end w-full text-gray-200">
-      <ul class="flex icons">
+    <div class="flex lg:flex-col justify-center items-center text-gray-200">
+      <ul class="flex">
         <li class="flex items-center content-center h-8 w-8 border-2 rounded-full m-1"><a href="#" class="m-auto">T</a></li>
         <li class="flex items-center content-center h-8 w-8 border-2 rounded-full m-1"><a href="#" class="m-auto">G</a></li>
         <li class="flex items-center content-center h-8 w-8 border-2 rounded-full m-1"><a href="#" class="m-auto">E</a></li>
