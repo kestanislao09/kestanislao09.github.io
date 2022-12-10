580187c169131d27b9cc72211b866e0e7e552bb3
Keith Estanislao <kestanislao09@outlook.com>
2022-12-10 03:51:00 -0800

    styling fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="bg-slate-600 h-20 w-20 mb-6 rounded-full">
       <img class="h-full w-full rounded-full" href="#"/>
     </div>
-    <h1 class="sm:max-lg:bg-black/60 rounded sm:max-lg:px-8 text-center text-2xl lg:text-xl text-gray-200"><strong>I am Keith Estanislao.</strong> Developer, gamer, and father</h1>
+    <h1 class="bg-black/60 lg:bg-black/0 rounded px-8 lg:px-0 text-center lg:text-right text-2xl lg:text-xl text-gray-200"><strong>I am Keith Estanislao.</strong> Dev, gamer, and father.</h1>
   </div>
 </template>
 
