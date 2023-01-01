db065e5fcb0367ab0fdc35e4a00b5e0ace61fff8
Keith Estanislao <kestanislao09@outlook.com>
2023-01-01 01:44:31 -0800

    github link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,11 @@
     <div class="bg-slate-600 h-20 w-20 mb-6 rounded-full">
       <img class="h-full w-full rounded-full" href="#"/>
     </div>
-    <h1 class="bg-black/60 lg:bg-black/0 rounded lg:px-0 text-center lg:text-right text-2xl lg:text-xl text-gray-200"><strong>I am Keith Estanislao.</strong><br> Dev, gamer, and father.</h1>
+    <h1 class="bg-black/60 lg:bg-black/0 rounded 
+               px-4 lg:px-0 text-center text-2xl 
+               lg:text-right lg:text-xl text-gray-200">
+      <strong>I am Keith Estanislao.</strong><br> Jack of many,<br> Master of some.
+    </h1>
   </div>
 </template>
 
